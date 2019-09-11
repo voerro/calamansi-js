e836c2a609912ae8653a965a55e06f3e24868993
Alexander Zavyalov <whmordred@gmail.com>
2019-09-11 09:04:32 +0800

    wip

--- src/calamansi/Calamansi.js
+++ src/calamansi/Calamansi.js
@@ -1,9 +1,21 @@
 class Calamansi
 {
     constructor(el) {
+        this.initialized = false;
+
         this.el = el;
         this.id = this.generateUniqueId();
         this.skinPath = el.attributes.skin ? el.attributes.skin.value : 'default';
+
+        try {
+            this.options = el.attributes.options
+                ? this.parseOptions(el.attributes.options.value)
+                : null;
+        } catch (error) {
+            console.error('Could not initialize a Calamansi instance');
+
+            return;
+        }
 
         this.eventListeners = {
             initialized: [],
@@ -20,24 +32,41 @@ class Calamansi
         const elements = document.querySelectorAll('v-calamansi');
 
         // Initialize all the player instances
-        elements.forEach(item => {
-            calamansis.push(new Calamansi(item));
+        elements.forEach(el => {
+            calamansis.push(new Calamansi(el));
         });
 
         return calamansis;
+    }
+
+    parseOptions(string) {
+        try {
+            string = string.replace(/'/g, '"');
+
+            return JSON.parse(string);
+        } catch (error) {
+            throw error;
+        }
     }
 
     async init() {
         this.loadSkinCss(this.skinPath);
         const skin = await this.fetchSkin(this.skinPath);
 
-        const wrapper = document.createElement('div');
+        let wrapper = document.createElement('div');
+        wrapper.innerHTML = skin;
+        
+        if (wrapper.firstChild.dataset.noWrapper) {
+            wrapper = wrapper.firstChild;
+        }
+
         wrapper.classList.add('calamansi');
         wrapper.id = this.id;
-        wrapper.innerHTML = skin;
 
         this.el.parentNode.replaceChild(wrapper, this.el);
         this.el = wrapper;
+
+        this.initialized = true;
 
         this.emit('initialized', this);
     }
@@ -84,6 +113,11 @@ class Calamansi
      * @param {*} callback 
      */
     on(event, callback) {
+        // Sometimes the player initialization might fail
+        if (!this.initialized) {
+            return;
+        }
+
         // Ignore inexisting event types
         if (!this.eventListeners[event]) {
             return;
