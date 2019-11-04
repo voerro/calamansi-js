401ad45b51e718c2576d1bf7f161dd1203ce9d55
Alexander Zavyalov <whmordred@gmail.com>
2019-11-04 13:41:56 +0800

    Nerios skin - a mobile/tables skin; Fixes in old skins

---
+++ dist/skins/nerios/skin.js
@@ -0,0 +1,20 @@
+; (function () {
+    CalamansiEvents.on('initialized', function (instance) {
+        var player = instance.el;
+
+        if (player.matches('.calamansi-skin--nerios')) {
+            if (document.querySelectorAll(`script[src="https://unpkg.com/simplebar@4.2.3/dist/simplebar.min.js"]`).length == 0) {
+                var script = document.createElement('script');
+                script.setAttribute('src', 'https://unpkg.com/simplebar@4.2.3/dist/simplebar.min.js');
+                script.setAttribute('type', 'text/javascript');
+                document.querySelector('head').appendChild(script);
+            }
+
+            player.querySelectorAll('.control-toggle-playlist').forEach(function (el) {
+                el.addEventListener('change', function (e) {
+                    player.classList.toggle('show-playlist');
+                });
+            });
+        }
+    });
+})();
